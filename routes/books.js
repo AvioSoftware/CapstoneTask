b6142e11d612d865763452d20822fef3c72845d9
ithunter101 <104503223+ithunter101@users.noreply.github.com>
2022-11-23 05:03:22 +0300

    Removed some advanced

--- routes/books.js
+++ routes/books.js
@@ -3,16 +3,11 @@ const express = require("express");
 
 // Importing models
 const Book = require("../models/Book");
-// Importing utils
-const validateBookPlayload = require("../validation/book");
-const isEmpty = require("../utils/is-empty");
 
 const router = express.Router();
 
 // @route GET /books
-// @desc  Returns a list of all books (title & id) regardless of
-//        checkout status (i.e. both status and checked-out books should be included)
-//        and can be filtered by status
+// @desc  Returns a list of all books
 router.get("/", (req, res) => {
   Book.find()
     // .select("id title")
@@ -24,86 +19,17 @@ router.get("/", (req, res) => {
     });
 });
 
-// @route GET /books/:id
-// @desc  Returns all details for the book matching id, 404 if not found
-router.get("/:id", (req, res) => {
-  Book.findOne({ id: req.params.id }).then((result) => {
-    if (!result) {
-      return res.status(404).json({ msg: "not found" });
-    }
-    res.json({ msg: "ok", result });
-  });
-});
-
 // @route POST /books
-// @desc  Add a new book as described in request body (JSON),
-//        which includes id & status
+// @desc  Get a list of books filtered by status
 router.post("/", (req, res) => {
-  if (!isEmpty(req.body.status)) {
+  if (req.body.status != undefined) {
     Book.find({ status: req.body.status } /*, "id title"*/).then((result) => {
       if (!result) {
         return res.status(404).json({ msg: "not found" });
       }
       res.json({ msg: "ok", result });
     });
-  } else {
-    const { errors, isValid } = validateBookPlayload(req.body);
-    if (!isValid) return res.status(400).json(errors);
-    Book.findOne({ id: req.body.id }).then((result) => {
-      if (result) {
-        return res.status(403).json({ msg: "already exists" });
-      }
-
-      const newBook = new Book({
-        id: req.body.id,
-        title: req.body.title,
-        author: req.body.author,
-        publisher: req.body.publisher,
-        isbn: req.body.isbn,
-      });
-
-      newBook
-        .save()
-        .then((result) => res.status(201).json({ msg: "created", result }));
-    });
   }
-});
-
-// @route PUT /books
-// @desc  For book matching id, update any properties present in
-//        request body (JSON). Can be used to check in or out
-router.put("/:id", (req, res) => {
-  Book.findOne({ id: req.params.id }).then((result) => {
-    if (!result) {
-      return res.status(404).json({ msg: "not found" });
-    }
-
-    if (!isEmpty(req.body.title)) result.title = req.body.title;
-    if (!isEmpty(req.body.author)) result.author = req.body.author;
-    if (!isEmpty(req.body.publisher)) result.publisher = req.body.publisher;
-    if (!isEmpty(req.body.isbn)) result.isbn = req.body.isbn;
-    if (!isEmpty(req.body.status)) {
-      if (!req.body.status) {
-        result.status = false;
-        result.checkedOutBy = "";
-        result.dueDate = "";
-      } else {
-        if (!isEmpty(req.body.checkedOutBy)) {
-          result.checkedOutBy = req.body.checkedOutBy;
-          result.dueDate = isEmpty(req.body.dueDate)
-            ? Date.now()
-            : req.body.dueDate;
-          result.status = true;
-        } else {
-          result.status = false;
-          result.checkedOutBy = "";
-          result.dueDate = "";
-        }
-      }
-    }
-
-    result.save().then((result) => res.status(200).json({ msg: "ok", result }));
-  });
 });
 
 // @route POST /books/checkOut
@@ -114,11 +40,9 @@ router.post("/checkOut/:id", (req, res) => {
       return res.status(404).json({ msg: "not found" });
     }
 
-    if (!isEmpty(req.body.checkedOutBy)) {
+    if (req.body.checkedOutBy != undefined) {
       result.checkedOutBy = req.body.checkedOutBy;
-      result.dueDate = isEmpty(req.body.dueDate)
-        ? Date.now()
-        : req.body.dueDate;
+      result.dueDate = Date.now();
       result.status = true;
     } else {
       result.status = false;
@@ -143,20 +67,6 @@ router.post("/checkIn/:id", (req, res) => {
     result.dueDate = "";
 
     result.save().then((result) => res.status(200).json({ msg: "ok", result }));
-  });
-});
-
-// @route DELETE /books
-// @desc  Delete the book matching id (if it exits) regardless of checkout status
-router.delete("/:id", (req, res) => {
-  Book.findOne({ id: req.params.id }).then((result) => {
-    if (!result) {
-      return res.status(204).json({ msg: "no content" });
-    }
-
-    result
-      .remove()
-      .then((result) => res.status(200).json({ msg: "ok", result }));
   });
 });
 
--- utils/is-empty.js
+++
@@ -1,8 +0,0 @@
-module.exports = isEmpty = (data) => {
-  return (
-    data === undefined ||
-    data === null ||
-    (typeof data === "string" && data.trim() === "") ||
-    (typeof data === "object" && Object.keys(data).length === 0)
-  );
-};
